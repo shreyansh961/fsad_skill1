af108778f08dff8ab0c548bfcd30309888f8b913
Shreyansh <bindumishra830@gmail.com>
2026-01-19 15:30:54 +0530

    skill1

---
+++ src/main/java/klu/Hibernate_crud/employee.java
@@ -0,0 +1,25 @@
+package klu.Hibernate_crud;
+
+public class employee {
+	int id;
+	String name;
+	String gender;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+}
